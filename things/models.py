9f141cc007a0a5ce32515b6fda4f9dd44b3d87f4
that guy <user@DESKTOP-EOQPDVD.localdomain>
2023-10-03 00:06:27 +0100

    redefine model

--- things/models.py
+++ things/models.py
@@ -4,11 +4,11 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30,unique=True)
-    description = models.TextField(max_length=120,blank=True)
+    name = models.CharField(max_length=30,unique=True,blank=False)
+    description = models.CharField(max_length=120,blank=True)
     quantity = models.IntegerField(validators=[
             MaxValueValidator(100),
-            MinValueValidator(1)
+            MinValueValidator(0)
         ])
 
 
